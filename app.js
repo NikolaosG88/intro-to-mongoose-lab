ca7e34338043e7c492a95697e06071514592b00d
NikolaosG <Nikolaos_Gkazgkas@outlook.com>
2024-10-09 18:07:25 +0100

    Almost done some alterations

--- app.js
+++ app.js
@@ -30,9 +30,10 @@ const connect = async () => {
 const menu = () => {
     console.log( '1. Create a customer');
     console.log( '2. View all customers');
-    console.log( '3. Update a customer');
-    console.log( '4. Delete a customer');
-    console.log( '5. Quit');
+    console.log( '3. Find Customer');
+    console.log( '4. Update a customer');
+    console.log( '5. Delete a customer');
+    console.log( '6. Quit');
     
     const selection = parseInt(prompt('Please select one action from "1-5" to be performed '));
 
@@ -41,12 +42,14 @@ const menu = () => {
     } else if (selection === 2) {
         viewCustomer();
     } else if (selection === 3) {
-        updateCustomer();
+        findCustomer();
     } else if (selection === 4) {
-        deleteCustomer();
+        updateCustomer();
     } else if (selection === 5) {
+        deleteCustomer();
+    } else if (selection === 6) {
          process.exit();
-    } else if (selection >= 6 || selection === 0){
+    } else if (selection >= 7 || selection === 0){
         console.log('Enter valid number according to options!')
         menu();
     } else {
@@ -67,5 +70,41 @@ const createCustomer = async () => {
     console.log('New Customer created:', newCustomer);
     menu();
 }
+
+const viewCustomer = async () => {
+    const customers = await Customer.find({})
+    console.log('All customers;', customers);
+    menu();
+}
+
+const findCustomer = async () => {
+   let choice = prompt('How you want to access info by Name or Age?: ');
+    let customer
+    if ( choice === 'Name') {
+        const name = prompt('Enter Customer Name: ');
+        customer = await Customer.findOne({ name: name });
+    } else if ( choice === 'Age'){
+        const age = parseInt(prompt('Enter Customer Age: '));
+        customer = await Customer.findOne({ age: age })
+    } else if (choice !== 'Name' || 'Age') {
+        console.log('Please read instructions again!')
+       return findCustomer();
+    }
+
+    console.log('Customer found:', customer);
+    menu();
+}
+
+const deleteCustomer = async () => {
+    const name = prompt('Enter Customer Name to be deleted: ');
+    const removedCustomer = await Customer.findOneAndDelete({
+        name: name
+    });
+
+    console.log('Removed Customer:', removedCustomer);
+    menu();
+    }
+
+
 
   connect();
